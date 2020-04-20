95d987bd2cc60d7e796c633c100544f4f22d4338
matt8381 <malloyml@mail.uc.edu>
2020-04-20 16:02:41 -0400

    printvdeck

--- FinalProjectSolitair/main.cpp
+++ FinalProjectSolitair/main.cpp
@@ -4,61 +4,79 @@
 
 using namespace std;
 
-class deck{
-private:
-    deck();
-    void createdeck();
-};
-
-deck::deck(){
-    string type[]= {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-    string suit[]= {"Hearts", "Diamonds", "Spades", "Clubs"};
-}
-
-void deck::createdeck(){
-    for(int i=0)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 class card{
 private:
     string suit;
     string type;
-    int value;
+    //int value;
 public:
     card();
-    card(string cardsuit, string cardtype, int cardvalue);
-
+    card(string cardsuit, string cardtype);
+    string printCard();
 };
 card::card(){
 }
 
-card::card(string cardsuit, string cardtype, int cardvalue){
+card::card(string cardsuit, string cardtype){
     suit=cardsuit;
     type=cardtype;
-    value=cardvalue;
+    //value=cardvalue;
+}
+string card::printCard(){
+    return (suit + " of  " + type);
 }
 
-int main()
-{
 
+
+class deck{
+private:
+    //deck();
+    card *deckOfCards;
+public:
+    void printdeck();
+    deck();
+};
+
+deck::deck(){
+    string type[]= {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+    string suit[]= {"Hearts", "Diamonds", "Spades", "Clubs"};
+    deckOfCards=new card[52];
+    for(unsigned i=0; i<52 ;i++){
+        deckOfCards[i]= card (type [i%13], suit [i/13]);
+    }
+}
+
+void deck::printdeck(){
+    for (int i=0; i<52; i++){
+        cout<<deckOfCards[i].printCard()<<endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+deck deckOfCards;
+deckOfCards.printdeck();
 }
