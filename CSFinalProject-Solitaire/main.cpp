7e5c6cc1a02168163d7fce9aae2f8b534250b9ad
Nick Smith <nicksmith3e@icloud.com>
2020-04-20 13:57:24 -0400

    Initial commit

---
+++ CSFinalProject-Solitaire/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSFinalProject-Solitaire
+//
+//  Created by Nick Smith on 4/20/20.
+//  Copyright © 2020 Nicholas Smith & Matthew Malloy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
