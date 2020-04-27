7052853232b16c9a977fdfad8c6899d148483376
Nick Smith <nicksmith3e@icloud.com>
2020-04-26 23:15:24 -0400

    Update game.cpp

--- FinalProjectSolitair/solitaire/game.cpp
+++ FinalProjectSolitair/solitaire/game.cpp
@@ -65,7 +65,7 @@ void Game::playGame(){
     }
 }
 
-int Game::sumPile(vector<Card> deckOfCards){
+int Game::sumPile(vector<Card> & deckOfCards){
     int pileSum = 0;
     for(unsigned int i = 0; i < pile.size(); i++){
             pileSum += deckOfCards.at(i).getValue();
@@ -75,7 +75,7 @@ int Game::sumPile(vector<Card> deckOfCards){
 
 }
 
-bool Game::checkPrime(vector<Card> deckOfCards){
+bool Game::checkPrime(vector<Card> & deckOfCards){
         //returns true if the sum of the pile is a prime number.
         //else it returns false
     vector<int>primeNumber={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337};
